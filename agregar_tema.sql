92d0714b46d7dd22ba444f11fb247fc3008683f4
msventas2015-wq <msventas2015@gmail.com>
2026-08-01 20:22:58 -0300

    Delete agregar_tema.sql

--- agregar_tema.sql
+++
@@ -1,5 +0,0 @@
--- Agrega columna "tema" a la tabla proyectos
--- Pegá esto en el SQL Editor de Supabase y hacé click en Run
-
-ALTER TABLE proyectos
-  ADD COLUMN IF NOT EXISTS tema text DEFAULT 'clasico';
