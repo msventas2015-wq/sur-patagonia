73420b0d05e1a648f978fd94a4df63496cbc7ee7
msventas2015-wq <msventas2015@gmail.com>
2026-08-01 20:21:35 -0300

    Delete fix_antiguedad.sql

--- fix_antiguedad.sql
+++
@@ -1,3 +0,0 @@
--- Corregir tipo de columna antiguedad (estaba como bigint, debe ser TEXT)
-ALTER TABLE propiedades ALTER COLUMN antiguedad TYPE TEXT USING antiguedad::TEXT;
-ALTER TABLE propiedades ALTER COLUMN condicion TYPE TEXT USING condicion::TEXT;
