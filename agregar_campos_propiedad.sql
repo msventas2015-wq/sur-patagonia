fdf23252dc97b9bef164bbf8d38bae3553a620ac
msventas2015-wq <msventas2015@gmail.com>
2026-08-01 20:22:37 -0300

    Delete agregar_campos_propiedad.sql

--- agregar_campos_propiedad.sql
+++
@@ -1,5 +0,0 @@
--- Agregar campos nuevos a la tabla propiedades
-ALTER TABLE propiedades
-  ADD COLUMN IF NOT EXISTS antiguedad TEXT,
-  ADD COLUMN IF NOT EXISTS condicion TEXT,
-  ADD COLUMN IF NOT EXISTS apto_credito BOOLEAN DEFAULT false;
