e15b0122f74ecdca698a65f3e6b6b37df42bd776
msventas2015-wq <msventas2015@gmail.com>
2026-08-01 22:05:29 -0300

    Delete agregar_fondo.sql

--- agregar_fondo.sql
+++
@@ -1,4 +0,0 @@
--- Agregar campos para foto de fondo y opacidad por proyecto
-ALTER TABLE proyectos
-  ADD COLUMN IF NOT EXISTS foto_fondo text,
-  ADD COLUMN IF NOT EXISTS opacidad_fondo integer DEFAULT 7;
