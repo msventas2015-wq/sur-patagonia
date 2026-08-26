269cc58ef5fa0a5973417ac541aaf4b725e22e43
msventas2015-wq <msventas2015@gmail.com>
2026-08-25 21:26:50 -0300

    Add files via upload

--- admin/sw.js
+++ admin/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'sur-patagonia-admin-';
-const CACHE_NAME = CACHE_PREFIX + 'v7';
+const CACHE_NAME = CACHE_PREFIX + 'v8';
 
 const STATIC_ASSETS = [
   '/assets/admin-icon-192.png',
